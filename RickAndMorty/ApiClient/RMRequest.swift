cb677a8690709e9410ad22f07483863dcd616f50
Obinna <obinna.aguwa@gmail.com>
2023-02-01 23:15:49 +0100

    started RMRequest implementation

--- RickAndMorty/ApiClient/RMRequest.swift
+++ RickAndMorty/ApiClient/RMRequest.swift
@@ -9,5 +9,43 @@ import Foundation
 
 /// Object that represent a single API call
 final class RMRequest {
+    private struct Constants {
+        static let baseUrl = "https://rickandmortyapi.com/api"
+    }
+    
+    private let endPoint: RMEndpoint
+    
+    private let pathComponents: [String]
+    
+    private let queryParameters: [URLQueryItem]
+    
+    private var urlString: String {
+        var string = Constants.baseUrl
+        string += "/"
+        string += endPoint.rawValue
+        
+        if !pathComponents.isEmpty {
+            pathComponents.forEach { string += "/\($0)"}
+        }
+        
+//        if !pathComponents
+        
+        return string
+    }
+    
+    public var url: URL? {
+        return nil
+    }
+    
+    // MARK: - Public
+    public init(
+        endPoint: RMEndpoint,
+        pathComponents: [String] = [],
+        queryParameters: [URLQueryItem] = []
+    ) {
+        self.endPoint = endPoint
+        self.pathComponents = pathComponents
+        self.queryParameters = queryParameters
+    }
     
 }
