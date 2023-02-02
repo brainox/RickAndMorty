f0be1bf8517ebb80e9faed06cad5e7875c1cf353
Obinna <obinna.aguwa@gmail.com>
2023-02-02 14:33:17 +0100

    Create API Request

--- RickAndMorty/ApiClient/RMRequest.swift
+++ RickAndMorty/ApiClient/RMRequest.swift
@@ -9,38 +9,60 @@ import Foundation
 
 /// Object that represent a single API call
 final class RMRequest {
+    ///  API  constants
     private struct Constants {
         static let baseUrl = "https://rickandmortyapi.com/api"
     }
     
+    /// Desired endpoint
     private let endPoint: RMEndpoint
     
-    private let pathComponents: [String]
+    /// Path components for API, if any
+    private let pathComponents: Set<String>
     
+    /// Query arguments for API, if any
     private let queryParameters: [URLQueryItem]
     
+    /// constructed url for the api request in string format
     private var urlString: String {
         var string = Constants.baseUrl
         string += "/"
         string += endPoint.rawValue
         
         if !pathComponents.isEmpty {
-            pathComponents.forEach { string += "/\($0)"}
+            pathComponents.forEach {
+                string += "/\($0)"
+            }
         }
         
-//        if !pathComponents
-        
+        if !queryParameters.isEmpty {
+            string += "?"
+            let argumentString = queryParameters.compactMap {
+                guard let value = $0.value else { return nil}
+                return "\($0.name)=\(value)"
+            }.joined(separator: "&")
+            string += argumentString
+        }
         return string
     }
     
+    /// Computed and constructed API url
     public var url: URL? {
-        return nil
+        return URL(string: urlString)
     }
     
+    /// Desired http method
+    public let httpMethod = "GET"
+    
     // MARK: - Public
+    /// Construct request
+    /// - Parameters:
+    ///   - endPoint: Target endpoint
+    ///   - pathComponents: Collection of path components
+    ///   - queryParameters: Collection of query parameters
     public init(
         endPoint: RMEndpoint,
-        pathComponents: [String] = [],
+        pathComponents: Set<String> = [],
         queryParameters: [URLQueryItem] = []
     ) {
         self.endPoint = endPoint
